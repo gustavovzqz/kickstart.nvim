182c46a11c0ad9b06d8e38dc7af4f0fcaa044605
= <gustavovzqz.dev@gmail.com>
2024-12-11 20:12:26 -0300

    material darker

--- init.lua
+++ init.lua
@@ -795,16 +795,14 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'blazkowolf/gruber-darker.nvim',
+    'marko-cerovac/material.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'gruber-darker'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      vim.g.material_style = 'darker'
+      vim.cmd.colorscheme 'material'
     end,
   },
 
